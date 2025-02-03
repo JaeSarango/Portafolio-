0fcf9c93b7558f40ad461ae5a347f5393f1cad0f
JaeSarango <sarajae02@gmail.com>
2025-02-03 16:29:43 -0300

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,7 +56,7 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/JaeSarango" target="_blank">
+          <Link href="https://github.com/Jae-Sarango" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://www.linkedin.com/in/jaely-sarango/" target="_blank">
